2ee9c78acf3369c9c455f0dd16b4d39ea9c41d81
Sebastian Czapla <sebastian.czapla.567@gmail.com>
2026-01-14 17:05:15 +0100

    Fix contain to use lambdas

--- trap.h
+++ trap.h
@@ -6,27 +6,14 @@
 #include "yield.h"
 
 namespace thaumaturgy {
-    template <typename Fn>
-    inline yield contain(Fn&& fn) noexcept {
-        yield y;
+    template <typename F>
+    yield contain(yield& out, F&& fn) noexcept {
         try {
-            if constexpr (std::is_same_v<decltype(fn()), yield>) {
-                y |= fn();
-            } else {
-                fn();
-            }
+            fn();
+        } catch (...) {
+            out.set_state(yield_state::trap);
         }
-        catch (const std::exception&) {
-            y.set_state(yield_state::trap)
-             .set_origin(yield_origin::process)
-             .set_code(1);
-        }
-        catch (...) {
-            y.set_state(yield_state::trap)
-             .set_origin(yield_origin::process)
-             .set_code(2);
-        }
-        return y;
+        return out;
     }
 
     constexpr inline bool warded(const yield& y) noexcept {
